87fc2edab62a7d9a058c40c1c4f741ca80a1720c
JimHui0 <113490169+JimHui0@users.noreply.github.com>
2022-10-21 20:46:13 +0800

    Add files via upload

--- RMS_tb.v
+++ RMS_tb.v
@@ -8,10 +8,6 @@ module RMS_tb();
     
     RMS_top RMS_top1(.clk(clk), .rst(rst), .start(start), .a(a), .RMS_code(RMS_code), .done(done));
     
-    initial begin
-		$fsdbDumpfile("Simulation_Result/RMS_rtl.fsdb");
-		$fsdbDumpvars;
-	end
     
     always #5 clk <= ~clk;
     
@@ -21,40 +17,42 @@ module RMS_tb();
         rst = 1;
     #100
         rst = 0;
+        
         a = 8'd42;
         start = 1;
     #10
         start = 0;
+        
     #410
-        rst = 0;
         a = 8'd45;
         start = 1;
     #10
         start = 0;
+        
     #410
-        rst = 0;
         a = 8'd47;
         start = 1;
     #10
         start = 0;
+        
     #410
-        rst = 0;
         a = 8'd51;
         start = 1;
     #10
         start = 0;
+        
     #410
-        rst = 0;
         a = 8'd49;
         start = 1;
     #10
         start = 0;
+        
     #410
-        rst = 0;
         a = 8'd50;
         start = 1;
     #10
         start = 0;
+        
     #820
         $finish;
     end
